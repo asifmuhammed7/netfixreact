682cd1dedb71bbf1276ea00428c84a58112c0819
asif <asifmuhammedn7@gmail.com>
2023-12-23 12:38:47 +0530

    Banner & RowPost

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/Navbar/NavBar';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Banner/>
+      <RowPost/>
     </div>
   );
 }
